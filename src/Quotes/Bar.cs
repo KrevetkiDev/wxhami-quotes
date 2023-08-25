208afa1460bf13e761b61aaee7af0239cf910d9f
wxhami <d18102002@mail.ru>
2023-08-25 22:32:37 +0300

    bar characteristics add

---
+++ src/Quotes/Bar.cs
@@ -0,0 +1,15 @@
+﻿namespace Quotes;
+
+public class Bar
+{
+   public string Symbol;
+   public string Description;
+   public DateTime Date;
+   public DateTime Time;
+   public double Open;
+   public double High;
+   public double Low;
+   public double Close;
+   public int TotalVolume;
+
+}
